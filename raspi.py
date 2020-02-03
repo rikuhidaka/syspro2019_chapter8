b13881da180c9089f15c888d0d519dee488e348f
rikuhidaka <s16161@tokyo.kosen-ac.jp>
2020-02-03 15:12:04 +0900

    s16161

--- raspi.py
+++ raspi.py
@@ -34,10 +34,13 @@ def on_snapshot(doc_snapshot, changes, read_time):
         if led == "ON":
             print "ON"
             # ONにする処理
+	    GPIO.output(14,GPIO.HIGH)
+	    time.sleep(1)
         elif led == "OFF":
             print "OFF"
-            # OFFにする処理
-
+            # OFFにする
+	    GPIO.output(14,GPIO.LOW)
+	    time.sleep(1)
 
 on_ref = db.collection('led').where(u'led', u'==', u'ON')
 off_ref = db.collection('led').where(u'led', u'==', u'OFF')
@@ -45,23 +48,6 @@ off_ref = db.collection('led').where(u'led', u'==', u'OFF')
 # 監視を開始する
 doc_watch = on_ref.on_snapshot(on_snapshot)
 doc_watch = off_ref.on_snapshot(on_snapshot)
-
-# 温度湿度気圧センサから値を取得してFirestoreに送信する部分
-# 「'''」で囲まれた部分はコメントアウトされているので，参考にすること．
-'''
-while True:
-    #温度，湿度，気圧の値を取得する
-    #temp = 
-    #hum = 
-    #press = 
-
-    print("Temperature:%6.2f" %(temp))
-    print("Humidity:%6.2f" %(hum))
-    print("Pressure:%6.2f" %(press))
-    data = {"temp": temp, "hum": hum, "press":press}
-    db.collection('temperature').document(str(datetime.datetime.now())).set(data)
-    time.sleep(1)
-'''
 
 # 温度センサと接続できないうちはこの無限ループを使う
 while True:
